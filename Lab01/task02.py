535bd2510384ff117aaf87c907d429317adb2896
Tomasz Nowak <tomasz.nowak@mycit.ie>
2021-07-25 13:17:35 +0100

    Functions sing_verse(), second_last_verse() and last_verse() created and called

---
+++ Lab01/task02.py
@@ -0,0 +1,27 @@
+def sing_verse(bottles):
+    print(f'{bottles} green bottles hanging on the wall,')
+    print(f'{bottles} green bottles hanging on the wall,')
+    print('And if one green bottle should accidentally fall,')
+    print(f"There'll be {bottles - 1} green bottles hanging on the wall.")
+    print()
+
+
+def second_last_verse():
+    print(f'Two green bottles hanging on the wall,')
+    print(f'Two green bottles hanging on the wall,')
+    print('And if one green bottle should accidentally fall,')
+    print(f"There'll be 1 green bottle hanging on the wall.")
+
+
+def last_verse():
+    print(f'One green bottle hanging on the wall,')
+    print(f'One green bottle hanging on the wall,')
+    print('And if one green bottle should accidentally fall,')
+    print("There'll be no green bottles hanging on the wall.")
+
+
+for x in range(20, 2, -1):
+    sing_verse(x)
+second_last_verse()
+print()
+last_verse()
