046ea5b96312f14470ff7f27fb39bc0d0d42b37c
Tomasz Nowak <tomasz.nowak@mycit.ie>
2021-07-25 13:15:34 +0100

    Functions test0(), test1(), test3() and rocket() created and called

---
+++ Lab01/task01.py
@@ -0,0 +1,30 @@
+def test0():
+    print('   /\\')
+    print('  /  \\')
+    print(' /    \\')
+    print('/      \\')
+    print('+------+')
+
+
+def test1():
+    print("|      |")
+    print("|      |")
+    print("|      |")
+    print("|      |\n+------+")
+
+
+def test3():
+    print("  //\\\\")
+    print("  //\\\\")
+    print("  //\\\\")
+
+
+def rocket():
+    test0()
+    test1()
+    test1()
+    test1()
+    test3()
+
+
+rocket()
