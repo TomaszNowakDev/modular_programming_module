3ad6eb49bc24eeb4be0cd286877315c107e7b631
Tomasz Nowak <tomeknew1@gmail.com>
2021-08-04 22:12:20 +0100

    Methods __str__, and nct() created for class Car.

--- Lab07/car_class.py
+++ Lab07/car_class.py
@@ -3,3 +3,9 @@ class Car:
         self.make = mk
         self.model = md
         self.year = y
+
+    def __str__(self):
+        return f"{self.make} {self.model} ({self.year})"
+
+    def nct(self):
+        return self.year + 4
