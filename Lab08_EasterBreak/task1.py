6ba2cba6142e24fe8c54d6b875628c40d2339dfd
Tomasz Nowak <tomeknew1@gmail.com>
2021-08-11 22:05:31 +0100

    Functions oldest_person(), youngest_person() and children() created and called.

--- Lab08_EasterBreak/task1.py
+++ Lab08_EasterBreak/task1.py
@@ -18,6 +18,32 @@ def average(hum):
     return x
 
 
+def oldest_person(hum):
+    ages = [h.age for h in hum]
+    names = [name.name for name in hum]
+    old_age = max(ages)
+    print(f"Oldest person is/are: ")
+    for i in range(len(ages)):
+        if ages[i] == old_age:
+            print(f"\t{names[i]}")
+
+
+def youngest_person(hum):
+    ages = [h.age for h in hum]
+    names = [name.name for name in hum]
+    young = min(ages)
+    print(f"Youngest person is/are: ")
+    for i in range(len(ages)):
+        if ages[i] == young:
+            print(f"\t{names[i]}")
+
+
+def children(hum):
+    kids = [kid.name for kid in hum if kid.age < 18]
+    print(f"Children under 18: ")
+    return kids
+
+
 def main():
     with open("people.cvs") as connection:
         lines = connection.readlines()
@@ -31,6 +57,12 @@ def main():
 
     average_result = average(humans)
     print(f"Average age is: {average_result:.2f} years.")
+
+    oldest_person(humans)
+    youngest_person(humans)
+    kids = children(humans)
+    display(kids)
+    print()
 
 
 if __name__ == '__main__':
