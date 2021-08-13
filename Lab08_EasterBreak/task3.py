c6fc0ccad1c763105951eb4fac69fe29b63cd98e
Tomasz Nowak <tomeknew1@gmail.com>
2021-08-13 22:31:57 +0100

    Function main() created, objects class Book created from file books.txt.

--- Lab08_EasterBreak/task3.py
+++ Lab08_EasterBreak/task3.py
@@ -8,3 +8,20 @@ class Book:
     def __str__(self):
         return f"IBSN = {self.ISBN}, Title = {self.title}, Author = {self.author}, Price = {self.price}"
 
+
+def main():
+    with open("books.txt") as connection:
+        lines = connection.readlines()
+        books = []
+        for line in lines:
+            split_line = line.split(",")
+            ibsn = int(split_line[0])
+            title = split_line[1]
+            author = split_line[2]
+            price = int(split_line[3])
+            book = Book(ibsn, title, author, price)
+            books.append(book)
+
+
+if __name__ == '__main__':
+    main()
