c23ab1a6b68e250e78f1b81a38dc90a3dd643d19
Tomasz Nowak <tomeknew1@gmail.com>
2021-08-20 22:59:26 +0100

    Functions validation_for_main() and validation_for_input() created.

--- Lab08_EasterBreak/task5.py
+++ Lab08_EasterBreak/task5.py
@@ -2,9 +2,32 @@
 MENU = "Shapes:\n1. Square\n2. Rectangle\n3. Right-angled triangle\n4. Circle\n5. Quit"
 
 
+def validation_for_input(prompt):
+    while True:
+        try:
+            user_input = int(input(prompt))
+            if user_input > 0:
+                return user_input
+        except ValueError:
+            print("Whole positive numbers only please.")
+
+
+def validation_for_menu(prompt):
+    while True:
+        try:
+            choice = int(input(prompt))
+            if 0 < choice <= 5:
+                break
+            else:
+                print("Choose one of the options from 1 to 5.")
+        except ValueError:
+            print("Numbers only please!")
+    return choice
+
+
 def main():
     print(MENU)
-    choice_main = input("==> ")
+    choice_main = validation_for_menu("==> ")
 
     while choice_main != 5:
         if choice_main == 1:
@@ -20,7 +43,7 @@ def main():
             print("4. Circle")
 
         print(MENU)
-        choice_main = input("==> ")
+        choice_main = validation_for_menu("==> ")
     print("Thank you, goodbye.")
 
 
